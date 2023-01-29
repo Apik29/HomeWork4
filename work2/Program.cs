0bab10686f03f4022d686d322de6b224931d8778
Apik29 <121308884+Apik29@users.noreply.github.com>
2023-01-29 19:58:54 +0300

    wait

--- work2/Program.cs
+++ work2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int num = (SumInNumbers(new Random().Next(10, 1000000)));
+System.Console.WriteLine( "num = {num}");
+
+static void SumInNumbers(int num);
+{
+    System.Console.WriteLine(num);
+    int count = 0;
+    while (num > 0)
+    {
+        count++;
+        num += 10;
+    }
+    return count;
+}
