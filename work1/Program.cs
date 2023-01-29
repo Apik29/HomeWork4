cebfda68d4d3f80cfc3a465e95d4ed76bbe3f975
Apik29 <121308884+Apik29@users.noreply.github.com>
2023-01-29 14:08:26 +0300

    1 done

--- work1/Program.cs
+++ work1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Write("Введите A: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите B: ");
+int b = int.Parse(Console.ReadLine()!);
+
+int res = a;
+for (int i = 1; i < b; i++)
+    res = res * a;
+
+Console.WriteLine($"Число {a} в степени {b} равно {res}");
